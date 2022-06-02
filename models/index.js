7f93c223a77cb3f5761b763bf404f32d687127de
Adam Brock <a.paulbrock@gmail.com>
2022-06-02 17:17:53 -0400

    Added get routes for api/categories/ and api/categories:id

--- models/index.js
+++ models/index.js
@@ -1,23 +1,24 @@
-// import models
 const Product = require("./Product");
 const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE",
+
+Product.belongsToMany(Tag, {
+  through: { model: ProductTag, unique: false },
+  as: "products_in_category",
 });
 
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: { model: ProductTag, uniqe: false } });
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: { model: ProductTag, uniqe: false } });
+Tag.belongsToMany(Product, {
+  through: { model: ProductTag, unique: false },
+  as: "tags_in_product",
+});
 
 module.exports = {
   Product,
