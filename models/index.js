c0d7bcf1f1848f057bace1f0c4dbfac289304e75
Adam Brock <a.paulbrock@gmail.com>
2022-06-02 12:30:53 -0400

    Added code that defines the 4 tables and relates them.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,23 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: { model: ProductTag, uniqe: false } });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: { model: ProductTag, uniqe: false } });
 
 module.exports = {
   Product,
